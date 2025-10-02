2e9b4a58512cd8d982ebbf69d558bef75ac05fea
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-03 02:20:22 +0900

    BRR: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./Hangmandrawing";
+import { HangmanWord } from "./Hangmanword";
+import { Keyboard } from "./Keyboard";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessLetters, setGuessLetters] = useState<string[]>([]);
+  console.log(wordToGuess);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
